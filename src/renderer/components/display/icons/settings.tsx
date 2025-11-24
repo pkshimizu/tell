555a2f41420d3cf99d23dc5913512eef00a73ebf
shimizu.kenji <shimizu.kenji@noncore.net>
2025-11-24 21:44:32 +0900

    サイドバーナビゲーション機能の実装
    - react-iconsを追加してアイコンコンポーネント（Home、Settings）を作成
    - TanStack Routerと統合したサイドバーコンポーネントを実装
    - Listコンポーネントにリンク機能を追加
    - 設定ページルートを追加

---
+++ src/renderer/components/display/icons/settings.tsx
@@ -0,0 +1,5 @@
+import { IoSettingsOutline } from 'react-icons/io5'
+
+export default function SettingsIcon() {
+  return <IoSettingsOutline />
+}
--- src/renderer/routes/__root.tsx
+++ src/renderer/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import SideBar from '@renderer/features/system/side-bar'
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <SideBar />
       <Outlet />
       <TanStackRouterDevtools />
     </>
